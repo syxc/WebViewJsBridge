d71727de322228ddf7cbec9fb21535024b3110b8
syxc <gaibing2009@gmail.com>
2016-08-08 12:08:10 +0800

    Fixed 'arguments.callee is deprecated' issue

--- WebViewJsBridge/Source/WebViewJsBridge.js
+++ WebViewJsBridge/Source/WebViewJsBridge.js
@@ -2,11 +2,11 @@
   var messagingIframe,
   bridge = 'external',
   CUSTOM_PROTOCOL_SCHEME = 'jscall';
-  
+
   if (window[bridge]) {
     return;
   }
-  
+
   function _createQueueReadyIframe(doc) {
     messagingIframe = doc.createElement('iframe');
     messagingIframe.style.display = 'none';
@@ -16,11 +16,13 @@
   window[bridge] = {};
   var methods = [%@];
   for (var i = 0; i < methods.length; i++) {
-    var method = methods[i];
-    var code = "(window[bridge])[method] = function " + method + "() {messagingIframe.src = CUSTOM_PROTOCOL_SCHEME + ':' + arguments.callee.name + ':' + encodeURIComponent(JSON.stringify(arguments));}";
-    eval(code);
+    (function(index) {
+      var method = methods[index];
+      var code = "window[bridge][method] = function() {messagingIframe.src = CUSTOM_PROTOCOL_SCHEME + ':' + method + ':' + encodeURIComponent(JSON.stringify(arguments));}";
+      eval(code);
+    })(i)
   }
-  
+
   // 创建iframe，必须在创建external之后，否则会出现死循环
   _createQueueReadyIframe(document);
   // 通知js开始初始化
